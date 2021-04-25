d1e9eaea6d6a2a95a913b8e590af891496d33a85
Your Name <you@example.com>
2021-04-25 12:19:20 +0300

    add movetexture.cs, fix font

---
+++ Assets/Materials/MoveTexture.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTexture : MonoBehaviour
+{
+    // Scroll main texture based on time
+    // Or follow object
+
+    [SerializeField]
+    Vector2 _scrollSpeed;
+    Renderer _rend;
+    [SerializeField]
+    Transform _follow;
+
+    void Start()
+    {
+        _rend = GetComponent<Renderer> ();
+    }
+
+    void Update()
+    {
+        //_rend.material.mainTextureOffset = new Vector2(Time.time*_scrollSpeed[0],Time.time*_scrollSpeed[1]);
+        _rend.material.mainTextureOffset = new Vector2(_follow.position[0]*_scrollSpeed[0],_follow.position[1]*_scrollSpeed[1]);
+    }
+}
